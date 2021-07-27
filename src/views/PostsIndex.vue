19e0eea843bf945cd9ea2373b5aeede9bee00d58
Garrett <garrettodom97@gmail.com>
2021-07-27 12:20:29 -0500

    Adds dynamic class to index

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
-    <div class="cards" v-for="post in posts" v-bind:key="post.id">
-      <router-link class="routerLink" v-bind:to="`/posts/${post.id}`">
-        <div class="card mb-3" style="max-width: 540px">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-              <img :src="post.image" alt="No Image Found" />
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">{{ post.title }}</h5>
-                <p class="card-text">
-                  {{ post.body }}
-                </p>
+    <div class="cards">
+      <div
+        v-for="post in posts"
+        v-bind:key="post.id"
+        :class="{ selected: post === currentPost }"
+        v-on:mouseover="currentPost = post"
+      >
+        <router-link class="routerLink" v-bind:to="`/posts/${post.id}`">
+          <div class="card mb-3" style="max-width: 540px">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img :src="post.image" alt="No Image Found" />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{ post.title }}</h5>
+                  <p class="card-text">
+                    {{ post.body }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -29,6 +36,7 @@ export default {
   data: function () {
     return {
       posts: [],
+      currentPost: {},
     };
   },
   created: function () {
@@ -53,5 +61,9 @@ export default {
 
 .cards {
   align-content: center;
+}
+
+.selected {
+  background-color: cadetblue;
 }
 </style>
